0c06722c2ffea2b039740408b31f573b45887030
SkyBird233 <52884766+SkyBird233@users.noreply.github.com>
2026-05-22 16:18:45 +0800

    refactor: move patch folders

--- scripts/docker/build-core.sh
+++ scripts/docker/build-core.sh
@@ -3,14 +3,14 @@
 set -e
 
 BASE_DIR="$(pwd)"/libretro-super
-PATCH_DIR="$(pwd)"/patches/"$1"
+PATCH_DIR="$(pwd)"/cores/"$1"/patches
 
 export ARCH=loongarch64
 export HOST_CC='loongarch64-linux-gnu'
 
 if [[ -d "$PATCH_DIR" ]]; then
   cd $BASE_DIR/libretro-"$1"
-  git apply -v "$PATCH_DIR"/*.patch
+  git apply -v "$PATCH_DIR"/*
 fi
 
 cd $BASE_DIR 
